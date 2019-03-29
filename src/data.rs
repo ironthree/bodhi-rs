00a192b35fa6a66dd7181f4c754450f1c6eee084
Fabio Valentini <decathorpe@gmail.com>
2019-03-29 19:55:18 +0100

    data: initial struct definitions, derived from actual JSON responses

---
+++ src/data.rs
@@ -0,0 +1,303 @@
+use std::collections::HashMap;
+
+use serde_derive::Deserialize;
+
+
+// derived from actual bodhi JSON responses
+
+#[derive(Deserialize, Debug)]
+pub struct Build {
+    ci_url: Option<String>,
+    epoch: i32,
+    nvr: String,
+    release_id: i32,
+    signed: bool,
+    r#type: String,
+}
+
+
+// TODO: make private again
+#[derive(Deserialize, Debug)]
+pub struct BuildListPage {
+    builds: Vec<Build>,
+    page: i32,
+    pages: i32,
+    rows_per_page: i32,
+    total: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Comment {
+    anonymous: bool,
+    author: String,
+    bug_feedback: Vec<BugFeedback>,
+    id: i32,
+    karma: i32,
+    karma_critpath: i32,
+    testcase_feedback: Vec<TestCaseFeedback>,
+    text: String,
+    timestamp: String,
+    update: Update,
+    update_id: i32,
+    update_title: String,
+    user: User,
+    user_id: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct BugFeedback {
+    bug: Bug,
+    bug_id: i32,
+    comment_id: i32,
+    karma: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Bug {
+    bug_id: i32,
+    // feedback: Option<Vec<BugFeedback>>, ?? occurs in Update, but not in BugFeedBack (circular dependency?)
+    parent: bool,
+    security: bool,
+    title: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct TestCaseFeedback {
+    comment_id: i32,
+    karma: i32,
+    testcase: TestCase,
+    testcase_id: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct TestCase {
+    name: String,
+    package: Package,
+    package_id: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Package {
+    name: String,
+    // requirements: Option<?>,
+    // stack: Option<?>,
+    // stack_id: Option<i32>,
+    r#type: String,
+}
+
+
+// TODO: make private again
+#[derive(Deserialize, Debug)]
+pub struct CommentListPage {
+    comments: Vec<Comment>,
+    page: i32,
+    pages: i32,
+    rows_per_page: i32,
+    total: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Compose {
+    checkpoints: HashMap<String, String>,
+    content_type: String,
+    date_created: String,
+    error_message: String,
+    release: Release,
+    release_id: i32,
+    request: String,
+    security: bool,
+    state: String,
+    state_date: String,
+    update_summary: Vec<HashMap<String, String>>,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Release {
+    branch: String,
+    candidate_tag: String,
+    composes: Vec<Compose>,
+    composed_by_bodhi: bool,
+    dist_tag: String,
+    id_prefix: String,
+    long_name: String,
+    mail_template: String,
+    name: String,
+    override_tag: String,
+    pending_signing_tag: String,
+    pending_stable_tag: String,
+    pending_testing_tag: String,
+    stable_tag: String,
+    state: String,
+    testing_tag: String,
+    version: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct CSRF {
+    csrf_token: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Markdown {
+    html: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Override {
+    build: Build,
+    build_id: i32,
+    expiration_date: String,
+    expired_date: Option<String>,
+    notes: String,
+    nvr: String,
+    submission_date: String,
+    submitter: User,
+    submitter_id: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct OverrideListPage {
+    overrides: Vec<Override>,
+    page: i32,
+    pages: i32,
+    rows_per_page: i32,
+    total: i32,
+}
+
+
+// TODO: make private again
+#[derive(Deserialize, Debug)]
+pub struct PackageListPage {
+    packages: Vec<Package>,
+    page: i32,
+    pages: i32,
+    rows_per_page: i32,
+    total: i32,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct ReleaseListPage {
+    page: i32,
+    pages: i32,
+    releases: Vec<Release>,
+    rows_per_page: i32,
+    total: i32,
+}
+
+
+/*
+#[derive(Deserialize, Debug)]
+struct Stack {
+    description: String,
+    // groups: Vec<?>,
+    name: String,
+    // packages: Vec<?>,
+    requirements: String,
+    users: Vec<User>,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct StackListPage {
+    page: i32,
+    pages: i32,
+    rows_per_page: i32,
+    stacks: Vec<Stack>,
+    total: i32,
+}
+*/
+
+
+#[derive(Deserialize, Debug)]
+struct Update {
+    alias: String,
+    autokarma: bool,
+    bugs: Vec<Bug>,
+    builds: Vec<Build>,
+    close_bugs: bool,
+    comments: Vec<Comment>,
+    compose: Option<Compose>,
+    content_type: String,
+    critpath: bool,
+    date_approved: Option<String>,
+    date_modified: Option<String>,
+    date_pushed: Option<String>,
+    date_stable: Option<String>,
+    date_submitted: String,
+    date_testing: Option<String>,
+    display_name: String,
+    greenwave_summary_string: String,
+    // greenwave_unsatisfied_requirements: Option<?>,
+    karma: i32,
+    locked: bool,
+    meets_testing_requirements: bool,
+    notes: String,
+    old_updateid: Option<String>,
+    pushed: bool,
+    release: Release,
+    request: Option<String>,
+    require_bugs: bool,
+    require_testcases: bool,
+    requirements: String,
+    severity: String,
+    stable_karma: i32,
+    status: String,
+    submitter: String,
+    suggest: String,
+    test_cases: Vec<TestCase>,
+    test_gating_status: String,
+    title: String,
+    r#type: String,
+    unstable_karma: i32,
+    updateid: String,
+    url: String,
+    user: User,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct User {
+    avatar: String,
+    email: Option<String>,
+    groups: Vec<Group>,
+    id: i32,
+    name: String,
+    openid: String,
+    show_popups: bool,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct Group {
+    name: String,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct UserDetails {
+    urls: HashMap<String, String>,
+    user: User,
+}
+
+
+#[derive(Deserialize, Debug)]
+struct UserListPage {
+    page: i32,
+    pages: i32,
+    rows_per_page: i32,
+    total: i32,
+    users: Vec<User>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,195 @@
+extern crate reqwest;
+extern crate serde_derive;
+
+use std::collections::HashMap;
+
+use reqwest::Response;
+
+mod data;
+use data::*;
+
+
+pub struct BodhiService {
+    url: String,
+}
+
+
+impl BodhiService {
+    pub fn new(url: String) -> BodhiService {
+        BodhiService { url }
+    }
+
+    fn request(&self, path: &String, args: Option<HashMap<&str, String>>) -> Result<Response, String> {
+        let client = reqwest::Client::new();
+        let url = format!("{}/{}", &self.url, path);
+
+        let query: Vec<(&str, String)> = match args {
+            Some(mut map) => { map.drain().collect() }
+            None => { Vec::new() }
+        };
+
+        dbg!(&path);
+        dbg!(&query);
+
+        let mut response = match client.get(&url).query(&query).send() {
+            Ok(response) => response,
+            Err(error) => { return Err(format!("{:#?}", error)); }
+        };
+
+        if !response.status().is_success() {
+            return Err(format!("{:#?}", response.text()));
+        };
+
+        return Ok(response);
+    }
+
+    pub fn query_build_by_nvr(&self, nvr: &String) -> Result<Option<Build>, String> {
+        let path = format!("/builds/{}", nvr);
+
+        let mut response = self.request(&path, None)?;
+
+        let build: Option<Build> = match response.json() {
+            Ok(value) => value,
+            Err(error) => { return Err(format!("{:?}", error)); }
+        };
+
+        Ok(build)
+    }
+
+    pub fn query_build(
+        &self,
+        nvr: Option<String>,
+        packages: Option<Vec<String>>,
+        releases: Option<Vec<String>>,
+        updates: Option<Vec<String>>,
+        page: Option<i32>,
+        rows_per_page: Option<i32>,
+    ) -> Result<BuildListPage, String> {
+        let path = String::from("/builds/");
+
+        let mut args: HashMap<&str, String> = HashMap::new();
+
+        if let Some(nvr) = nvr {
+            args.insert("nvr", nvr);
+        }
+
+        if let Some(packages) = packages {
+            args.insert("packages", packages.join(","));
+        }
+
+        if let Some(releases) = releases {
+            args.insert("releases", releases.join(","));
+        }
+
+        if let Some(updates) = updates {
+            args.insert("updates", updates.join(","));
+        }
+
+        if let Some(page) = page {
+            args.insert("page", format!("{}", page));
+        }
+
+        if let Some(rpp) = rows_per_page {
+            args.insert("rows_per_page", format!("{}", rpp));
+        }
+
+        let mut response = self.request(&path, Some(args))?;
+
+        let builds: BuildListPage = match response.json() {
+            Ok(value) => value,
+            Err(error) => { return Err(format!("{:?}", error)); }
+        };
+
+        Ok(builds)
+    }
+
+    pub fn query_comment_by_id(&self, id: i32) -> Result<Option<Comment>, String> {
+        let path = format!("/comments/{}", id);
+
+        let mut response = self.request(&path, None)?;
+
+        let comment: Comment = match response.json() {
+            Ok(value) => value,
+            Err(error) => { return Err(format!("{:?}", error)); }
+        };
+
+        Ok(Some(comment))
+    }
+
+    pub fn query_comment(
+        &self,
+        like: Option<String>,
+        search: Option<String>,
+        page: Option<i32>,
+        rows_per_page: Option<i32>,
+        updates: Option<Vec<String>>,
+        packages: Option<Vec<String>>,
+        user: Option<Vec<String>>,
+        update_owner: Option<Vec<String>>,
+        ignore_user: Option<Vec<String>>,
+        anonymous: Option<bool>,
+        since: Option<String>,
+    ) -> Result<CommentListPage, String> {
+        let path = String::from("/comments/");
+
+        let mut args: HashMap<&str, String> = HashMap::new();
+
+        if let Some(like) = like {
+            args.insert("like", like);
+        }
+
+        if let Some(search) = search {
+            args.insert("search", search);
+        }
+
+        if let Some(page) = page {
+            args.insert("page", format!("{}", page));
+        }
+
+        if let Some(rpp) = rows_per_page {
+            args.insert("rows_per_page", format!("{}", rpp));
+        }
+
+        if let Some(updates) = updates {
+            args.insert("updates", updates.join(","));
+        }
+
+        if let Some(packages) = packages {
+            args.insert("packages", packages.join(","));
+        }
+
+        if let Some(user) = user {
+            args.insert("releases", user.join(","));
+        }
+
+        if let Some(owner) = update_owner {
+            args.insert("update_owner", owner.join(","));
+        }
+
+        if let Some(ignored) = ignore_user {
+            args.insert("ignore_user", ignored.join(","));
+        }
+
+        if let Some(anon) = anonymous {
+            args.insert("anonymous", format!("{}", anon));
+        }
+
+        if let Some(date) = since {
+            args.insert("since", date);
+        }
+
+        let mut response = self.request(&path, Some(args))?;
+
+        let comments: CommentListPage = match response.json() {
+            Ok(value) => value,
+            Err(error) => { return Err(format!("{:?}", error)); }
+        };
+
+        Ok(comments)
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
