8a20d4431b19a506405381378f87a0d42af0aa1a
Fabio Valentini <decathorpe@gmail.com>
2019-05-16 17:47:38 +0200

    service: refactor queries to allow for retrying after failures

--- src/service.rs
+++ src/service.rs
@@ -1,19 +1,23 @@
 use std::collections::HashMap;
 
 use reqwest::Response;
+use std::thread::sleep;
 use std::time::Duration;
 
 /// Always start with page 1 for multi-page queries.
 /// Everything else would be stupid.
-pub const DEFAULT_PAGE: i32 = 1;
+pub const DEFAULT_PAGE: u32 = 1;
 
 /// This constant defines how many items are queried every time for multi-page queries.
 /// The maximum is 100, the default is 20, and 50 seems a good compromise for speed.
-pub const DEFAULT_ROWS: i32 = 50;
+pub const DEFAULT_ROWS: u32 = 50;
 
 /// Specify a longer timeout duration (60 s) for bodhi requests.
-/// The `reqwest` default value of 30 s is a bit too short for long-running queries.
+/// The `reqwest` default value of 30 seconds is a bit too short for long-running queries.
 const REQUEST_TIMEOUT: Duration = Duration::from_secs(60);
+
+/// Specify a number of retries in case of connection failures.
+const REQUEST_RETRIES: u32 = 3;
 
 /// This struct represents a specific bodhi service, typically running remotely,
 /// although a local URL could be specified, as well.
@@ -25,17 +29,28 @@ const REQUEST_TIMEOUT: Duration = Duration::from_secs(60);
 pub struct BodhiService {
     url: String,
     timeout: Duration,
+    retries: u32,
 }
 
 impl BodhiService {
     /// This method constructs a new `BodhiService` instance.
     pub fn new(url: String) -> BodhiService {
-        BodhiService { url, timeout: REQUEST_TIMEOUT }
+        BodhiService {
+            url,
+            timeout: REQUEST_TIMEOUT,
+            retries: REQUEST_RETRIES,
+        }
     }
 
     /// This method allows to override the default timeout value (60 seconds).
     pub fn timeout(mut self, timeout: Duration) -> BodhiService {
         self.timeout = timeout;
+        self
+    }
+
+    /// This method allows to override the default number of retries (3).
+    pub fn retries(mut self, retries: u32) -> BodhiService {
+        self.retries = retries;
         self
     }
 
@@ -45,6 +60,7 @@ impl BodhiService {
         path: &str,
         args: Option<HashMap<&str, String>>,
     ) -> Result<Response, String> {
+        // construct custom client (with longer timeout)
         let client = match reqwest::Client::builder().timeout(self.timeout).build() {
             Ok(client) => client,
             Err(error) => {
@@ -52,6 +68,7 @@ impl BodhiService {
             }
         };
 
+        // construct url and query
         let url = format!("{}/{}", &self.url, path);
 
         let query: Vec<(&str, String)> = match args {
@@ -59,14 +76,39 @@ impl BodhiService {
             None => Vec::new(),
         };
 
-        let response = match client.get(&url).query(&query).send() {
-            Ok(response) => response,
-            Err(error) => {
-                return Err(format!("{:#?}", error));
-            }
-        };
+        // retry in case of connection failures
+        let mut retries = REQUEST_RETRIES;
 
-        Ok(response)
+        while retries > 0 {
+            if let Ok(response) = client
+                .get(&url)
+                .header(reqwest::header::USER_AGENT, "bodhi-rs")
+                .query(&query)
+                .send()
+            {
+                match response.content_length() {
+                    None => {
+                        // response is empty
+                        sleep(Duration::from_secs(1));
+                        retries -= 1;
+                    }
+                    Some(_len) => {
+                        // return the first valid response
+                        return Ok(response);
+                    }
+                }
+            } else {
+                // take a breath, and keep on trying (or not)
+                sleep(Duration::from_secs(1));
+                retries -= 1;
+            }
+        }
+
+        // fail if the connection keeps failing
+        Err(format!(
+            "Failed to query bodhi instance ({} retries).",
+            REQUEST_RETRIES
+        ))
     }
 
     // TODO: OpenID client authentication
