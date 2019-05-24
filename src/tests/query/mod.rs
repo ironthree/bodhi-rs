91c11496f90de7cda673b6457e96433466f9566a
Fabio Valentini <decathorpe@gmail.com>
2019-05-24 21:17:12 +0200

    tests: refactor module

---
+++ src/tests/query/mod.rs
@@ -0,0 +1,17 @@
+use std::time::Duration;
+
+// Longer timeout value for tests, since these queries can take a long time
+const TEST_TIMEOUT: Duration = Duration::from_secs(120);
+
+// More retries for running the tests, since they fail quite often
+const TEST_RETRIES: u32 = 5;
+
+mod builds;
+mod comments;
+mod csrf;
+mod overrides;
+mod packages;
+mod releases;
+mod stacks;
+mod updates;
+mod users;
