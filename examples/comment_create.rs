c3e5162dad87f124990f8c2293de2b2904cde8ab
Fabio Valentini <decathorpe@gmail.com>
2019-09-30 12:03:38 +0200

    examples: add simple examples for creating test comment and override

---
+++ examples/comment_create.rs
@@ -0,0 +1,49 @@
+use std::io::{stdin, stdout, Write};
+
+use bodhi::create::{CommentBuilder, Karma};
+use bodhi::data::*;
+use bodhi::service::BodhiServiceBuilder;
+
+fn main() -> Result<(), String> {
+    let mut username = String::new();
+    let mut password = String::new();
+
+    print!("FAS username: ");
+    stdout().flush().unwrap();
+    if let Err(error) = stdin().read_line(&mut username) {
+        return Err(error.to_string());
+    }
+    let username = username.trim().to_string();
+
+    print!("FAS password: ");
+    stdout().flush().unwrap();
+    if let Err(error) = stdin().read_line(&mut password) {
+        return Err(error.to_string());
+    }
+    let password = password.trim().to_string();
+
+    let mut bodhi = BodhiServiceBuilder::new(String::from(FEDORA_BODHI_STAGING_URL))
+        .build()
+        .unwrap();
+
+    let auth = bodhi.authenticate(username, password);
+
+    if let Err(error) = auth {
+        return Err(format!("Failed to authenticate: {:?}", error));
+    }
+
+    let new_comment = CommentBuilder::new(String::from("FEDORA-2019-e39d4910c6"))
+        .text(String::from("Test comment from bodhi-rs."))
+        .karma(Karma::Positive);
+
+    let response = new_comment.create(&bodhi);
+
+    match response {
+        Ok(new_comment) => {
+            println!("New comment created:");
+            println!("{:#?}", new_comment);
+            Ok(())
+        }
+        Err(error) => Err(format!("{:?}", error)),
+    }
+}
---
+++ examples/override_create.rs
@@ -0,0 +1,47 @@
+use std::io::{stdin, stdout, Write};
+
+use bodhi::create::OverrideBuilder;
+use bodhi::data::*;
+use bodhi::service::BodhiServiceBuilder;
+
+fn main() -> Result<(), String> {
+    let mut username = String::new();
+    let mut password = String::new();
+
+    print!("FAS username: ");
+    stdout().flush().unwrap();
+    if let Err(error) = stdin().read_line(&mut username) {
+        return Err(error.to_string());
+    }
+    let username = username.trim().to_string();
+
+    print!("FAS password: ");
+    stdout().flush().unwrap();
+    if let Err(error) = stdin().read_line(&mut password) {
+        return Err(error.to_string());
+    }
+    let password = password.trim().to_string();
+
+    let mut bodhi = BodhiServiceBuilder::new(String::from(FEDORA_BODHI_STAGING_URL))
+        .build()
+        .unwrap();
+
+    let auth = bodhi.authenticate(username, password);
+
+    if let Err(error) = auth {
+        return Err(format!("Failed to authenticate: {:?}", error));
+    }
+
+    let new_override = OverrideBuilder::new(
+        String::from("libcloudproviders-0.3.0-1.fc30"),
+        String::from("Test buildroot override created by bodhi-rs."),
+        String::from("2019-08-31"),
+    );
+
+    let response = new_override.create(&bodhi);
+
+    match response {
+        Ok(_) => Ok(()),
+        Err(error) => Err(format!("{:?}", error)),
+    }
+}
